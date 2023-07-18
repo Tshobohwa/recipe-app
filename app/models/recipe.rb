11631f86410472a5b280e01b8254b9f9f5c6442a
FrankMugagga <frank.mugagga@gmail.com>
2023-07-18 19:07:44 +0300

    Add validations and relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many: :recipe_foods, class_name: 'RecipeFood', foreign_key: :recipe_id: :destroy
+  has_many: :foods, through: :recipe_foods
+
+  validates :name, :description, presence: true
+  validates :preparation_time, :cooking_time, numericality: { greater_than_or_equal_to:0 }
+  validates :public, inclusion: [true, false]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :recipes, clas_name: 'Recipe', foreign_key: :user_id: :destroy
+    has_many :foods, class_name: 'Food' foreign_key: :user_id: :destroy
+
+    validates :name, presence: true
 end
