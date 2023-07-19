22945de29d47fb07eaf36d7db712168324ce6bc7
FrankMugagga <frank.mugagga@gmail.com>
2023-07-19 09:34:01 +0300

    Add testing Gem and model tests

---
+++ spec/models/recipe_food_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+  before :each do
+  end
+
+  it 'Should be valid with a value for quantity' do
+
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'Should be valid with a name' do
+    user = User.new(name: 'Lumbuye')
+    expect(user).to be_valid
+  end
+
+  it 'Should be invalid without a name' do
+    user = User.new(name: '')    
+    expect(user).not_to be_valid
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  it 'Should be invlid if variable is removed' do
+    user = User.new()
+    expect(user).not_to be_valid
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+end
