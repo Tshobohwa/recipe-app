dc0030e53a4488e770677c797f1b5f3e68989b36
FrankMugagga <frank.mugagga@gmail.com>
2023-07-19 09:35:27 +0300

    Set up model relations and validations

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -3,4 +3,6 @@ class RecipeFood < ApplicationRecord
   belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
 
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to:0 }
+  validates :food_id, presence: true
+  validates :recipe_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :recipes, clas_name: 'Recipe', foreign_key: :user_id: :destroy
-    has_many :foods, class_name: 'Food' foreign_key: :user_id: :destroy
+    has_many :recipes, class_name: 'Recipe', foreign_key: :user_id, dependent: :destroy
+    has_many :foods, class_name: 'Food', foreign_key: :user_id, dependent: :destroy
 
     validates :name, presence: true
 end
