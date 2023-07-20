874c61e3b86cf6af2aaffe84b487d0aa20ef676a
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-20 19:26:11 +0200

    Create foods list with CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :foods, only: [:index, :new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
+  delete 'foods/:id', to: 'foods#delete', as: 'delete_food'
 end
