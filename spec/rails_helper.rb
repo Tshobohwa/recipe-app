dffa1b4f372cce36f56315ac2b0f00853f78027c
FrankMugagga <frank.mugagga@gmail.com>
2023-07-20 20:20:15 +0300

    create for integration test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -60,4 +62,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Rspec.config do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
 end
