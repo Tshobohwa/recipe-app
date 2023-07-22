d51b11497a4c465d2de4bad4066b64f997d30279
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-22 19:03:42 +0200

    Add shopping list

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,5 +1,11 @@
 class ShoppingListsController < ApplicationController
-    def index
-        @recipe = Recipe.includes(recipe_foods: :food).find(params[:id])
+  def index
+    @recipe = Recipe.includes(recipe_foods: :food).find(params[:id])
+    @amount_of_food_to_buy = @recipe.recipe_foods.length
+    @total_value_of_food_needed = 0
+    @recipe.recipe_foods.each do |recipe_food|
+      total_value = recipe_food.quantity * recipe_food.food.price
+      @total_value_of_food_needed += total_value
     end
+  end
 end
