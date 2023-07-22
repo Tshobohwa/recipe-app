5e9c913ca0e7d3aa6388c1ce25c4c7e48e0311ca
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-22 20:26:20 +0200

    Update public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.includes(:user).where(public: true).order(created_at: :desc)
+    @public_recipes = Recipe.includes(:user, recipe_foods: :food).where(public: true).order(created_at: :desc)
   end
 end
